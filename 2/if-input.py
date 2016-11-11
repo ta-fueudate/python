4f71b272f5b77d6d8377afc1ed785a3e8dff0e86
tatsuya-furudate <marifuru0902@gmail.com>
2016-11-11 22:23:45 +0900

    study python 1 if 2 if input

---
+++ 2/if-input.py
@@ -0,0 +1,5 @@
+num=int(input("what number？"))
+if num % 2 == 0:
+    print("even")
+else:
+    print("Odd")
---
+++ 2/if.py
@@ -0,0 +1,5 @@
+temperature=10
+if temperature > 15:
+     print("cold water")
+else:
+    print("Hot water")
