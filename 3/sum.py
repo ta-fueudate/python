ebd009bb43c5695fc8ffad384ecf1717947a9f82
tatsuya-furudate <marifuru0902@gmail.com>
2016-11-17 22:18:49 +0900

    python study 1 sum

---
+++ 3/sum.py
@@ -0,0 +1,10 @@
+points=[88,76,100,24,12,89,22]
+sum_v=sum(points)
+ave_v=sum_v/len(points)
+print("total point is",sum_v)
+print("averrage is",ave_v)
+
+
+fruits=["Apple","Banana","orange","abogado"]
+for i in fruits:
+    print("["+i+"]")
