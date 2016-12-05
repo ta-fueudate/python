0793d1c44182fc3e384a52f01bedf2eecde6b5f7
tatsuya-furudate <marifuru0902@gmail.com>
2016-12-05 17:19:24 +0900

    tkinter don't move but its is file is moved

---
+++ 2/t.py
@@ -0,0 +1,11 @@
+import sys
+import tkinter
+
+
+root = tkinter.Tk()
+root.title(u"Software Title")
+root.geometry("400x300")
+#label
+Static1 = tkinter.Label(text=u'test')
+Static1.pack()
+root.mainloop()
