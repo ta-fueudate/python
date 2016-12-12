c6b8d651545455f45ec43f3978941aeb9ac7aaf0
tatsuya <marifuru0902@gmail.com>
2016-12-12 10:49:44 +0900

    edit change

--- desktop/dialog-file.py
+++ desktop/dialog-file.py
@@ -1,7 +1,8 @@
+# ダイアログを表示するために必要なモジュール
 import tkinter.filedialog as fd
 
+# ファイル選択ダイアログを表示する
 path = fd.askopenfilename(
-    title="set your file",
+    title="処理対象のファイルを指定してください",
     filetypes=[('HTML','.html')])
-
 print(path)
